e4c7d6fff4eea5a5979131035888c8e33ffc4455
Dan Tereschenko <daniel@tereschenko.info>
2017-10-11 22:19:25 +0300

    tests

--- tests/ImmutableTest.php
+++ tests/ImmutableTest.php
@@ -4,7 +4,12 @@ use PHPUnit\Framework\TestCase;
 
 class ImmutableTest extends TestCase
 {
-    const VALUE = 1234;
+    const
+        SIMPLE_DESCRIBE = '((10RUB) * 5 + 5USD - 3RUB) * 2',
+        SIMPLE_COLLAPSE = ['RUB' => 94, 'USD' => 10],
+        SIMPLE_COURSE   = ['RUB' => 1, 'USD' => 63.23],
+        SIMPLE_ASFLOAT  = 726.3
+    ;
 
     /**
      * @test
@@ -13,8 +18,8 @@ class ImmutableTest extends TestCase
     {
         $a = (CCY::RUB(10)->mul(5)->add(CCY::USD(5))->sub(CCY::RUB(3)))->mul(2);
 
-        $this->assertEquals('((10RUB) * 5 + 5USD - 3RUB) * 2', $a->describe());
-        $this->assertEquals(['RUB' => 94, 'USD' => 10], $a->collapse());
-        $this->assertEquals(726.3, $a->asFloat(['RUB' => 1, 'USD' => 63.23]));
+        $this->assertEquals(self::SIMPLE_DESCRIBE, $a->describe());
+        $this->assertEquals(self::SIMPLE_COLLAPSE, $a->collapse());
+        $this->assertEquals(self::SIMPLE_ASFLOAT,  $a->asFloat(self::SIMPLE_COURSE));
     }
 }
